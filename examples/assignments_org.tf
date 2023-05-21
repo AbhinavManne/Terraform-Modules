b035800e926cfdbf7c3e2564407ae0f393c8014e
aswini-chirumamilla <99787922+aswini-chirumamilla@users.noreply.github.com>
2023-05-21 17:21:51 +0530

    Add files via upload

---
+++ examples/assignments_org.tf
@@ -0,0 +1,109 @@
+##################
+# General
+##################
+module "org_mg_whitelist_regions" {
+  source            = "..//modules/def_assignment"
+  definition        = module.whitelist_regions.definition
+  assignment_scope  = data.azurerm_management_group.org.id
+  assignment_effect = "Deny"
+
+  assignment_parameters = {
+    "listOfRegionsAllowed" = [
+      "UK South",
+      "UK West",
+      "Global"
+    ]
+  }
+
+  assignment_metadata = {
+    version   = "1.0.0"
+    category  = "Batch"
+    propertyA = "A"
+    propertyB = "B"
+  }
+}
+
+
+##################
+# Security Center
+##################
+module "org_mg_configure_asc_initiative" {
+  source               = "..//modules/set_assignment"
+  initiative           = module.configure_asc_initiative.initiative
+  assignment_scope     = data.azurerm_management_group.org.id
+  assignment_effect    = "DeployIfNotExists"
+  skip_remediation     = var.skip_remediation
+  skip_role_assignment = var.skip_role_assignment
+
+  role_assignment_scope = data.azurerm_management_group.team_a.id # using explicit scopes
+  role_definition_ids = [
+    data.azurerm_role_definition.contributor.id # using explicit roles
+  ]
+
+  assignment_parameters = {
+    workspaceId           = local.dummy_resource_ids.azurerm_log_analytics_workspace
+    eventHubDetails       = local.dummy_resource_ids.azurerm_eventhub_namespace_authorization_rule
+    securityContactsEmail = "admin@cloud.com"
+    securityContactsPhone = "44897654987"
+  }
+}
+
+
+##################
+# Monitoring
+##################
+module "org_mg_platform_diagnostics_initiative" {
+  source               = "..//modules/set_assignment"
+  initiative           = module.platform_diagnostics_initiative.initiative
+  assignment_scope     = data.azurerm_management_group.org.id
+  skip_remediation     = true
+  skip_role_assignment = false
+
+  role_definition_ids = [
+    data.azurerm_role_definition.contributor.id # using explicit roles
+  ]
+
+  non_compliance_messages = {
+    null                                        = "The Default non-compliance message for all member definitions"
+    "DeployApplicationGatewayDiagnosticSetting" = "The non-compliance message for the deploy_application_gateway_diagnostic_setting definition"
+  }
+
+  assignment_parameters = {
+    workspaceId                                        = local.dummy_resource_ids.azurerm_log_analytics_workspace
+    storageAccountId                                   = local.dummy_resource_ids.azurerm_storage_account
+    eventHubName                                       = local.dummy_resource_ids.azurerm_eventhub_namespace
+    eventHubAuthorizationRuleId                        = local.dummy_resource_ids.azurerm_eventhub_namespace_authorization_rule
+    metricsEnabled                                     = "True"
+    logsEnabled                                        = "True"
+    effect_DeployApplicationGatewayDiagnosticSetting   = "DeployIfNotExists"
+    effect_DeployEventhubDiagnosticSetting             = "DeployIfNotExists"
+    effect_DeployFirewallDiagnosticSetting             = "DeployIfNotExists"
+    effect_DeployKeyvaultDiagnosticSetting             = "AuditIfNotExists"
+    effect_DeployLoadbalancerDiagnosticSetting         = "AuditIfNotExists"
+    effect_DeployNetworkInterfaceDiagnosticSetting     = "AuditIfNotExists"
+    effect_DeployNetworkSecurityGroupDiagnosticSetting = "AuditIfNotExists"
+    effect_DeployPublicIpDiagnosticSetting             = "AuditIfNotExists"
+    effect_DeployStorageAccountDiagnosticSetting       = "DeployIfNotExists"
+    effect_DeploySubscriptionDiagnosticSetting         = "DeployIfNotExists"
+    effect_DeployVnetDiagnosticSetting                 = "AuditIfNotExists"
+    effect_DeployVnetGatewayDiagnosticSetting          = "AuditIfNotExists"
+  }
+}
+
+
+##################
+# Storage
+##################
+module "org_mg_storage_enforce_https" {
+  source            = "..//modules/def_assignment"
+  definition        = module.storage_enforce_https.definition
+  assignment_scope  = data.azurerm_management_group.org.id
+  assignment_effect = "Deny"
+}
+
+module "org_mg_storage_enforce_minimum_tls1_2" {
+  source            = "..//modules/def_assignment"
+  definition        = module.storage_enforce_minimum_tls1_2.definition
+  assignment_scope  = data.azurerm_management_group.org.id
+  assignment_effect = "Deny"
+}
---
+++ policies/Monitoring/README.md
@@ -0,0 +1,9 @@
+# Monitoring Policies
+
+Azure Monitor Policies
+
+## References
+
+[List of Azure Policy built-in definitions for Azure Monitor](https://docs.microsoft.com/en-us/azure/azure-monitor/samples/policy-reference)
+
+[Create diagnostic settings at scale using Azure Policy](https://docs.microsoft.com/en-us/azure/azure-monitor/essentials/diagnostic-settings-policy)
